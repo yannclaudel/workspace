b35a81ad705c0d37c21b5bdd435f9fe393ddb56c
Yann Claudel <yclaudel@gmail.com>
2019-09-18 10:04:32 +0200

    algo

--- algo/src/EditDistance.java
+++ algo/src/EditDistance.java
@@ -1,17 +1,56 @@
 import java.util.*;
 
 class EditDistance {
-  public static int EditDistance(String s, String t) {
-    //write your code here
-    return 0;
-  }
-  public static void main(String args[]) {
-    Scanner scan = new Scanner(System.in);
 
-    String s = scan.next();
-    String t = scan.next();
+	private static int getMin(int a, int b, int c) {
+		if (a <= b && a <= c)
+			return a;
+		else if (b <= a && b <= c)
+			return b;
+		else
+			return c;
+	}
 
-    System.out.println(EditDistance(s, t));
-  }
+	public static int EditDistance(String s, String t) {
+		int n = s.length();
+		int m = t.length();
+		int[][] d = new int[n + 1][m + 1];
+
+		for (int i = 0; i < n + 1; i++) {
+			d[i][0] = i;
+		}
+		for (int i = 0; i < m + 1; i++) {
+			d[0][i] = i;
+		}
+		for (int j = 1; j < m + 1; j++) {
+			for (int i = 1; i < n + 1; i++) {
+				int insertion = d[i][j - 1] + 1;
+				int deletion = d[i - 1][j] + 1;
+				int match = d[i - 1][j - 1];
+				int mismatch = d[i - 1][j - 1] + 1;
+				if (s.charAt(i - 1) == t.charAt(j - 1)) {
+					d[i][j] = getMin(insertion, deletion, match);
+				} else {
+					d[i][j] = getMin(insertion, deletion, mismatch);
+				}
+			}
+		}
+		for (int j = 1; j < m + 1; j++) {
+			for (int i = 1; i < n + 1; i++) {
+				System.out.print(d[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		return d[n][m];
+	}
+
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+
+		String s = scan.next();
+		String t = scan.next();
+
+		System.out.println(EditDistance(s, t));
+	}
 
 }
--- algo/src/PrimitiveCalculator.java
+++ algo/src/PrimitiveCalculator.java
@@ -1,56 +1,70 @@
 import java.util.*;
 
 public class PrimitiveCalculator {
-	
+	private static int getMin(int a, int b, int c) {
+		if (a <= b && a <= c)
+			return a;
+		else if (b <= a && b <= c)
+			return b;
+		else
+			return c;
+	}
+
 	private static List<Integer> optimal_sequence(int n) {
 		List<Integer> sequence = new ArrayList<Integer>();
 		int[] path = new int[n + 1];
 		path[0] = 0;
-		path[1] = 1;
+		path[1] = 0;
 
-		for (int i = 2; i < n+1; i++) {
-			path[i] = path[i-1]+1;
-			if (i % 2 == 0 && path[i/2]+1<path[i]) {
-				path[i] = path[i/2]+1;
-			}
-			if (i % 3 == 0 && path[i/3]+1<path[i]) {
-				path[i] = path[i/3]+1;
-			}
+		for (int i = 2; i < n + 1; i++) {
+			int m1 = path[i - 1] + 1;
+			int m2 = (i % 2 == 0 ? path[i / 2] + 1 : Integer.MAX_VALUE);
+			int m3 = (i % 3 == 0 ? path[i / 3] + 1 : Integer.MAX_VALUE);
+			path[i] = getMin(m1, m2, m3);
 		}
-		
+
 		int i = n;
 		sequence.add(n);
-		while(i>1) {
-			int min1 = path[]
+		while (i > 1) {
+			int m1 = path[i - 1];
+			int m2 = (i % 2 == 0 ? path[i / 2] : Integer.MAX_VALUE);
+			int m3 = (i % 3 == 0 ? path[i / 3] : Integer.MAX_VALUE);
+			if (m3 <= m1 && m3 <= m2) {
+				i = i / 3;
+			} else if (m2 <= m3 && m2 <= m1) {
+				i = i / 2;
+			} else if (m1 <= m3 && m1 <= m2) {
+				i = i - 1;
+			}
+			sequence.add(i);
 		}
 		Collections.reverse(sequence);
-        return sequence;
+		return sequence;
 	}
-	
-    private static List<Integer> optimal_sequence2(int n) {
-        List<Integer> sequence = new ArrayList<Integer>();
-        while (n >= 1) {
-            sequence.add(n);
-            if (n % 3 == 0) {
-                n /= 3;
-            } else if (n % 2 == 0) {
-                n /= 2;
-            } else {
-                n -= 1;
-            }
-        }
-        Collections.reverse(sequence);
-        return sequence;
-    }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        List<Integer> sequence = optimal_sequence(n);
-        System.out.println(sequence.size() - 1);
-        for (Integer x : sequence) {
-            System.out.print(x + " ");
-        }
-    }
+	private static List<Integer> optimal_sequence2(int n) {
+		List<Integer> sequence = new ArrayList<Integer>();
+		while (n >= 1) {
+			sequence.add(n);
+			if (n % 3 == 0) {
+				n /= 3;
+			} else if (n % 2 == 0) {
+				n /= 2;
+			} else {
+				n -= 1;
+			}
+		}
+		Collections.reverse(sequence);
+		return sequence;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		List<Integer> sequence = optimal_sequence(n);
+		System.out.println(sequence.size() - 1);
+		for (Integer x : sequence) {
+			System.out.print(x + " ");
+		}
+	}
 }
-
